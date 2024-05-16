fb4c3a4a20bec193b335a6ca1fd77f5abf72e581
Vishalkumarjha72 <110764035+Vishalkumarjha72@users.noreply.github.com>
2024-05-16 16:04:00 +0530

    1. Project setup

--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -3,7 +3,7 @@ import { signup,login,logout } from "../controllers/authcontrollers.js";
 
 const router = express.Router();
 
-router.get("signup",signup);
+router.get("signup",signup); 
 router.get("login",login);
 router.get("logout",logout);
 export default router;
